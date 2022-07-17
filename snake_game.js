fa0009554987f57e2726c5e943347ccaddfbf773
Knoble-Solutions <106287477+Knoble-Solutions@user.noreply.github.com>
2022-07-17 18:32:53 -0400

    3rd commit

--- snake_game.js
+++ snake_game.js
@@ -4,7 +4,7 @@ const LINE_PIXEL_COUNT = 40
 const TOTAL_PIXEL_COUNT = LINE_PIXEL_COUNT**2
 
 //Track scores to display to user
-let toatlFoodEaten = 0
+let totalFoodEaten = 0
 let totalDistanceTraveled = 0;
 
 //Shorten reference to game board
@@ -37,3 +37,99 @@ const LEFT_DIR = 37
 const UP_DIR = 38
 const RIGHT_DIR = 39
 const DOWN_DIR = 40
+
+let snakeCurrentDirection = RIGHT_DIR
+
+// Make sure that the user input is valid and change snake direction variable
+const changeDirection = newDirectionCode => {
+    if (newDirectionCode == snakeCurrentDirection) return;
+
+    if (newDirectionCode == LEFT_DIR && snakeCurrentDirection !== RIGHT_DIR) {
+        snakeCurrentDirection = newDirectionCode
+
+    } else if (newDirectionCode == UP_DIR && snakeCurrentDirection !== DOWN_DIR) {
+        snakeCurrentDirection = newDirectionCode
+
+    } else if (newDirectionCode == RIGHT_DIR && snakeCurrentDirection !== LEFT_DIR) {
+        snakeCurrentDirection = newDirectionCode
+
+    } else if (newDirectionCode == DOWN_DIR && snakeCurrentDirection !== UP_DIR) {
+        snakeCurrentDirection = newDirectionCode
+    }
+}
+
+// Set starting point for snake on load
+let currentHeadPosition = TOTAL_PIXEL_COUNT/2 
+
+// Set initial length
+let snakeLength = 200
+
+// Start moving snake
+const moveSnake = () => {
+    switch (snakeCurrentDirection) {
+        case LEFT_DIR:
+            --currentHeadPosition
+            const isHeadAtLeft = currentHeadPosition % LINE_PIXEL_COUNT == LINE_PIXEL_COUNT - 1 || currentHeadPosition < 0
+
+            if (isHeadAtLeft) {
+                currentHeadPosition = currentHeadPosition + LINE_PIXEL_COUNT
+            }
+        break;
+        case RIGHT_DIR:
+            ++currentHeadPosition
+            const isHeadAtRight = currentHeadPosition % LINE_PIXEL_COUNT == 0
+            if (isHeadAtRight) {
+                currentHeadPosition = currentHeadPosition - LINE_PIXEL_COUNT
+            }
+            break;
+        case UP_DIR :
+            currentHeadPosition = currentHeadPosition - LINE_PIXEL_COUNT
+            const isHeadAtTop = currentHeadPosition < 0
+            if (isHeadAtTop) {
+                currentHeadPosition = currentHeadPosition + TOTAL_PIXEL_COUNT
+            }
+            break;
+        case DOWN_DIR: 
+            currentHeadPosition = currentHeadPosition + LINE_PIXEL_COUNT
+            const isHeadAtBottom = currentHeadPosition > TOTAL_PIXEL_COUNT - 1
+            if (isHeadAtBottom) {
+                currentHeadPosition = currentHeadPosition - TOTAL_PIXEL_COUNT
+            }
+            break;
+            default:
+            break;
+    }
+
+    //Access the correct pixel within the HTML collection
+    let nextSnakeHeadPixel = gameBoardPixels[currentHeadPosition]
+
+    //Check if snake head is about to intersect with its own body
+    if (nextSnakeHeadPixel.classList.contains("snakeBodyPixel")) {
+        clearInterval(moveSnakeInterval)
+        alert(`You have eaten ${totalFoodEaten} food and traveled ${totalDistanceTraveled} blocks.`)
+        window.location.reload()
+    }
+
+
+    // assuming an empty pixel, add snakee body styling
+    nextSnakeHeadPixel.classList.add("snakeBodyPixel")
+
+    // remove snake styling to keep snake appropriate length
+    setTimeout(() => {
+        nextSnakeHeadPixel.classList.remove("snakeBodyPixel")
+    }, snakeLength)
+
+    // Describe what to do if the snake encounters a food pixel
+    if (currentHeadPosition == currentFoodPosition) {
+      totalFoodEaten++
+      document.getElementById("pointEarned").innerText = totalFoodEaten
+      snakeLength = snakeLength + 100
+      createFood()
+    }
+}
+
+createGameBoardPixels();
+
+createFood();
+
+let moveSnakeInterval = setInterval(moveSnake, 100)
